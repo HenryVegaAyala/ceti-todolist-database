ad8fc8d904ea229b63ac984ce108aac63621094a
Henry Vega <hp.vega21@gmail.com>
2025-11-24 18:22:48 -0500

    Refactor GetAllTodosUseCase to use Lombok for constructor injection

--- src/main/java/com/mvc/todolist/application/usecase/todo/GetAllTodosUseCase.java
+++ src/main/java/com/mvc/todolist/application/usecase/todo/GetAllTodosUseCase.java
@@ -2,18 +2,16 @@ package com.mvc.todolist.application.usecase.todo;
 
 import com.mvc.todolist.domain.model.Todo;
 import com.mvc.todolist.domain.port.TodoRepositoryPort;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class GetAllTodosUseCase {
 
     private final TodoRepositoryPort todoRepositoryPort;
-
-    public GetAllTodosUseCase(TodoRepositoryPort todoRepositoryPort) {
-        this.todoRepositoryPort = todoRepositoryPort;
-    }
 
     public List<Todo> execute() {
         return todoRepositoryPort.findAll();
